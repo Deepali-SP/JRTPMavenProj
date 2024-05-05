8dd08328aba414b1916d95d2f511c8939ce3bc32
Deepali TL <ranijorve@gmail.com>
2024-05-05 18:22:13 +0530

    changes done in method name sum() to sub()

--- MavenProj02/src/main/java/com/app/Calculations.java
+++ MavenProj02/src/main/java/com/app/Calculations.java
@@ -4,8 +4,11 @@ public class Calculations{
 	public static int add(int a, int b) {
 		return a+b;
 	}
-	public static int sum(int a, int b) {
+	public static int sub(int a, int b) {
 		System.out.println("sub ::");
 		return a-b;
 	}
+	
+	
+
 }
