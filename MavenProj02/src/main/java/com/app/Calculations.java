630a94abaf4433204d5db43fefaa19750a908c5c
rani <rani@192.168.1.8>
2024-05-05 17:40:51 +0530

    added space by TL

--- MavenProj02/src/main/java/com/app/Calculations.java
+++ MavenProj02/src/main/java/com/app/Calculations.java
@@ -1,7 +1,7 @@
 package com.app;
 
 public class Calculations{
-	public static int add(int a,int b) {
+	public static int add(int a, int b) {
 		return a+b;
 	}
 	public static int sum(int a, int b) {
