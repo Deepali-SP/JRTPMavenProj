b334ce297eee6e32f5cd6e2e9d8a475a9d3b12ed
Rani <rani@Apples-MacBook-Pro.local>
2024-05-05 17:42:53 +0530

    Dev1 added mul()

--- MavenProj02/src/main/java/com/app/Calculations.java
+++ MavenProj02/src/main/java/com/app/Calculations.java
@@ -1,7 +1,10 @@
 package com.app;
 
 public class Calculations{
-	public static int add(int a,int b) {
+	public static int add(int a, int b) {
 		return a+b;
+	}
+	public static int mul(int a, int b) {
+		return a*b;
 	}
 }
