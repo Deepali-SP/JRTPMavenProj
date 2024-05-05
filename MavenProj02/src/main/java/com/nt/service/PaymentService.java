f5b6cbad406d658853e91d753c9dd83836feb39e
Rani <rani@Apples-MacBook-Pro.local>
2024-05-05 18:03:34 +0530

    Dev1 added upiPayment()

--- MavenProj02/src/main/java/com/nt/service/PaymentService.java
+++ MavenProj02/src/main/java/com/nt/service/PaymentService.java
@@ -5,4 +5,7 @@ public class PaymentService {
 	public static void cardPayment(Long cardNo, int amt) {
 		System.out.println("Payment done by Card");
 	}
+	public static void upiPayment(Long pin, int amt) {
+		System.out.println("Payment done by UPI");
+	}
 }
